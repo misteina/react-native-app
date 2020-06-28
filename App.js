b2673da0104f46a88a872b34a2db641c78b0968d
Enyinna <enyinna.tech@gmail.com>
2020-06-28 19:44:01 +0100

    28-6-2020 19:43

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
